36571a7940578684eb36dd124a818af762f2a8e9
hyebinpark <hoonywhitesocks@gmail.com>
2025-10-29 21:28:22 +0900

    feat: 공통 Footer 컴포넌트 구현

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="space-y-6 bg-neutral-100 px-4 py-6">
+      <h2 className="text-lg">이건뭐약</h2>
+      <ul className="flex gap-6 text-sm">
+        <li className="hover:underline">개인정보처리방침</li>
+        <li className="hover:underline">이용약관</li>
+      </ul>
+      <p className="text-sm text-neutral-500">© 2025 OZ Main Project 5팀. All rights reserved.</p>
+    </footer>
+  );
+}
