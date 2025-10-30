c4e6d993b1d6e148312d9adac359a07b06d18715
hyebinpark <hoonywhitesocks@gmail.com>
2025-10-30 14:31:29 +0900

    design: 요소 간 간격 및 텍스트 색상 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center bg-neutral-100">
-      <div className="w-full max-w-[1440px] space-y-6 px-4 py-6">
-        <h2 className="text-lg">이건뭐약</h2>
-        <ul className="flex gap-6 text-sm">
+      <div className="grid w-full max-w-[1440px] gap-6 px-4 py-6">
+        <h2 className="text-lg text-neutral-900">이건뭐약</h2>
+        <ul className="flex gap-6 text-sm text-neutral-900">
           <li className="hover:underline">개인정보처리방침</li>
           <li className="hover:underline">이용약관</li>
         </ul>
