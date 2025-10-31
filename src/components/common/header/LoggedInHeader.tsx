ed4a0b5c4081645305544d26174b06115f1b89ab
hyebinpark <hoonywhitesocks@gmail.com>
2025-10-31 14:42:31 +0900

    feat: 로그인 상태에 따라 Header 컴포넌트 LoggedInHeader와 UnLoggedInHeader로 분리(#15)

---
+++ src/components/common/header/LoggedInHeader.tsx
@@ -0,0 +1,41 @@
+import Button from "@/components/common/Button";
+import { LogOutIcon, User2Icon } from "lucide-react";
+import { Link } from "react-router";
+
+export default function LoggedInHeader() {
+  return (
+    <header className="flex justify-center bg-green-600 text-neutral-50">
+      <div className="flex w-full max-w-[1440px] items-center justify-between p-3">
+        <h1>
+          <Link to="/" className="text-lg text-neutral-50 sm:text-2xl sm:font-medium">
+            이건뭐약
+          </Link>
+        </h1>
+        <nav className="flex gap-3">
+          {/* 마이페이지 */}
+          <Link
+            to="/mypage"
+            className="inline-flex h-10 w-10 items-center justify-center rounded-lg border border-green-600 bg-neutral-50 text-green-600 hover:bg-neutral-200 sm:h-14 sm:w-auto"
+            aria-label="마이페이지"
+          >
+            <User2Icon className="sm:hidden" />
+            <span className="hidden text-lg text-neutral-900 sm:inline sm:px-6">마이페이지</span>
+          </Link>
+
+          {/* 로그아웃 */}
+          <Button
+            variant={"primaryOutline"}
+            size="sm"
+            className="inline-flex h-10 w-10 items-center justify-center p-0 text-green-600 sm:hidden"
+            aria-label="로그아웃"
+          >
+            <LogOutIcon />
+          </Button>
+          <Button variant={"primaryOutline"} size="lg" className="hidden sm:block">
+            로그아웃
+          </Button>
+        </nav>
+      </div>
+    </header>
+  );
+}
