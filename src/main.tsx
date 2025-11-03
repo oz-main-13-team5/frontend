75d56d1a13f881bd3a74dd8d208dd9af71a1379a
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-03 14:32:01 +0900

    chore: set up msw (#44)

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,26 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import Providers from "@/components/Providers.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Providers>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Providers>
-  </StrictMode>
-);
+async function enableMocking() {
+  const { worker } = await import("@/mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+const root = createRoot(document.getElementById("root")!);
+
+enableMocking().then(() => {
+  root.render(
+    <StrictMode>
+      <Providers>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Providers>
+    </StrictMode>
+  );
+});
