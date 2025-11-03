b492271355a4aca57138e11d0b4114fca5bb46e0
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-03 11:22:27 +0900

    feat: ImageSearchBarModal UI (#41)

---
+++ src/components/image-search-bar-modal/ImageSearchBarModal.tsx
@@ -0,0 +1,34 @@
+import Button from "@/components/common/Button";
+import Input from "@/components/common/Input";
+
+interface ImageSearchBarModalProps {}
+
+export default function ImageSearchBarModal({}: ImageSearchBarModalProps) {
+  return (
+    <div className="flex w-full max-w-2xl flex-col items-center justify-center gap-5 rounded-xl border border-neutral-400 bg-neutral-50 p-6">
+      <span className="text-2xl font-medium text-neutral-900">이미지 검색</span>
+
+      {/* image drop zone */}
+      <div className="flex h-36 w-full items-center justify-center rounded-lg bg-neutral-300 p-2">
+        <span className="text-neutral-600">여기로 이미지를 드래그하거나 파일을 업로드하세요</span>
+      </div>
+
+      {/* divder */}
+      <div className="relative w-full border border-green-600">
+        <span className="absolute -top-3 left-1/2 -translate-x-1/2 bg-neutral-50 px-1 text-neutral-900">
+          또는
+        </span>
+      </div>
+
+      {/* image url link */}
+      <div className="flex h-14 w-full items-center gap-2">
+        <Input
+          className="h-full flex-1 gap-0"
+          inputClassName="h-full"
+          placeholder="이미지 링크 붙여넣기"
+        />
+        <Button className="h-full">검색</Button>
+      </div>
+    </div>
+  );
+}
