edb0d8c653fe4389a7e9aafee212e13289ddbf28
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-03 14:39:38 +0900

    test: add msw test code (#44)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,1 +1,8 @@
-export const handlers = [];
+import { MSW_BASE_URL } from "@/constants/url-constants";
+import { http, HttpResponse } from "msw";
+
+const getTestMSW = http.get(`${MSW_BASE_URL}/get-test`, () => {
+  return HttpResponse.text("msw is working!");
+});
+
+export const handlers = [getTestMSW];
