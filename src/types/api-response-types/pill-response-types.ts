5b71a26e142d42aa1db1b5a2c03a45529d97ab22
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-04 14:30:01 +0900

    feat: 의약품 api response 타입 선언 (#57)

---
+++ src/types/api-response-types/pill-response-types.ts
@@ -0,0 +1,14 @@
+export interface PillList {
+  page: number;
+  limit: number;
+  total: number;
+  pills: Pill[];
+}
+
+export interface Pill {
+  item_seq: string;
+  item_name: string;
+  efcy_qesitm: string;
+  entp_name: string;
+  item_image_url: string;
+}
