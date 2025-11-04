2594594e5bf22db68bd7826d1db8dc5aa8757617
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-04 14:07:53 +0900

    feat: 로그인 페이지 컴포넌트 생성 및 라우트 추가 (#59)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import RootLayout from "@/components/layouts/RootLayout";
 import Home from "@/pages/Home";
 import PillList from "@/pages/PillList";
 import SignUp from "@/pages/SignUp";
+import Login from "@/pages/Login";
 import { Route, Routes } from "react-router";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/pill" element={<PillList />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
