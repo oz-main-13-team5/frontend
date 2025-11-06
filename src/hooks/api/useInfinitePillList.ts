009e6f3a50b16ff1b6f482cb124efd8919d33f13
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-06 13:24:51 +0900

    feat: useInfinitePillList에 검색 로직 추가 (#69)

--- src/hooks/api/useInfinitePillList.ts
+++ src/hooks/api/useInfinitePillList.ts
@@ -1,6 +1,7 @@
-import { PILL_LIST_PAGE_LIMIT } from "@/constants/api-constants";
+import { PILL_LIST_PAGE_LIMIT, PILL_SEARCH_OPTION_MAP } from "@/constants/api-constants";
 import { MSW_BASE_URL } from "@/constants/url-constants";
 import type { PillList } from "@/types/api-response-types/pill-response-types";
+import type { pillSearchOptionFrontend } from "@/types/types";
 import {
   useInfiniteQuery,
   type InfiniteData,
@@ -13,21 +14,46 @@ type useInfinitePillListOption = Omit<
   "queryFn" | "queryKey" | "initialPageParam" | "getNextPageParam"
 >;
 
-export default function useInfinitePillList(options?: useInfinitePillListOption) {
-  return useInfiniteQuery({
-    queryKey: ["pill"],
-    queryFn: async ({ pageParam }) => {
-      const res = await axios.get(`${MSW_BASE_URL}/pills`, {
-        params: {
-          page: pageParam,
-        },
-      });
+interface PillSearchParam {
+  queryParamKey: pillSearchOptionFrontend;
+  queryParamValue: string;
+}
 
-      return res.data;
+export default function useInfinitePillList(
+  pillSearchParam?: PillSearchParam,
+  options?: useInfinitePillListOption
+) {
+  return useInfiniteQuery({
+    queryKey: pillSearchParam
+      ? ["pill", `${pillSearchParam.queryParamKey}=${pillSearchParam.queryParamValue.trim()}`]
+      : ["pill"],
+    queryFn: async ({ pageParam }) => {
+      if (pillSearchParam) {
+        const { queryParamKey, queryParamValue } = pillSearchParam;
+
+        const trimmedValue = queryParamValue?.trim() ?? "";
+
+        const res = await axios.get(`${MSW_BASE_URL}/pills/search`, {
+          params: {
+            page: pageParam,
+            [PILL_SEARCH_OPTION_MAP[queryParamKey]]: trimmedValue,
+          },
+        });
+
+        return res.data;
+      } else {
+        const res = await axios.get(`${MSW_BASE_URL}/pills`, {
+          params: {
+            page: pageParam,
+          },
+        });
+
+        return res.data;
+      }
     },
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
-      if (lastPage.page * PILL_LIST_PAGE_LIMIT > lastPage.total) {
+      if (lastPage.page * PILL_LIST_PAGE_LIMIT >= lastPage.total) {
         return null;
       }
 
