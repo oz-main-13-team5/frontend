f98eed283ad9959d7088cdf57167e3ecb19a3b81
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-06 13:53:11 +0900

    feat: 의약품 검색 값 전역 상태 (#69)

---
+++ src/hooks/stores/usePillSearchStore.ts
@@ -0,0 +1,16 @@
+import type { pillSearchOptionFrontend } from "@/types/types";
+import { create } from "zustand";
+
+interface PillSearchState {
+  queryParamKey: pillSearchOptionFrontend;
+  queryParamValue: string;
+  setQueryParamKey: (key: pillSearchOptionFrontend) => void;
+  setQueryParamValue: (value: string) => void;
+}
+
+export const usePillSearchStore = create<PillSearchState>()((set) => ({
+  queryParamKey: "itemName",
+  queryParamValue: "",
+  setQueryParamKey: (key) => set(() => ({ queryParamKey: key })),
+  setQueryParamValue: (value) => set(() => ({ queryParamValue: value })),
+}));
