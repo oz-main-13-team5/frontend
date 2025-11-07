e2ed101fd5cc4e2766976cf60de1ef183f563507
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-07 15:31:31 +0900

    feat: 북마크 최대 개수 초과 알림 모달 (#75)

--- src/components/common/Bookmark.tsx
+++ src/components/common/Bookmark.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/Modal";
 import useToggleBookmark from "@/hooks/api/useToggleBookmark";
 import { cn } from "@/libs/utils";
 import { BookmarkIcon } from "lucide-react";
@@ -15,6 +16,7 @@ export default function Bookmark({
   ...props
 }: BookmarkProps) {
   const [isBookmarked, setIsBookmarked] = useState(initialState);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { mutate } = useToggleBookmark(
     { isDelete: isBookmarked, id: pillId },
@@ -23,8 +25,7 @@ export default function Bookmark({
         //실패하면 낙천적 UI 정정
         if (data && !data?.success) {
           setIsBookmarked((prev) => !prev);
-          //TODO: 이 메세지를 보여주는 모달로 변경
-          alert("북마크는 최대 20개까지만 가능합니다!");
+          setIsModalOpen(true);
         }
       },
     }
@@ -37,15 +38,26 @@ export default function Bookmark({
   };
 
   return (
-    <button
-      onClick={handleButtonClick}
-      className={cn(
-        "flex size-10 items-center justify-center rounded-lg border border-green-600 sm:size-14",
-        className
-      )}
-      {...props}
-    >
-      <BookmarkIcon className="text-green-600" fill={isBookmarked ? "#16a34a" : "#fafafa"} />
-    </button>
+    <>
+      <button
+        onClick={handleButtonClick}
+        className={cn(
+          "flex size-10 items-center justify-center rounded-lg border border-green-600 sm:size-14",
+          className
+        )}
+        {...props}
+      >
+        <BookmarkIcon className="text-green-600" fill={isBookmarked ? "#16a34a" : "#fafafa"} />
+      </button>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+        }}
+        title="북마크 최대 개수 초과"
+        description="북마크는 최대 20개까지만 가능합니다."
+      />
+    </>
   );
 }
