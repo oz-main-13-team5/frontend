19c0e6edfe2b1b0fafb2563dc36f3f0508acc8a9
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-10 13:29:06 +0900

    feat: 컴포넌트 간 간격 수정(#84)

--- src/pages/MyPageEdit.tsx
+++ src/pages/MyPageEdit.tsx
@@ -5,8 +5,8 @@ import PasswordEditForm from "@/components/my-page/edit/PasswordEditForm";
 export default function MyPageEdit() {
   return (
     <div className="flex justify-center p-5">
-      <div className="flex w-full flex-col items-center justify-start gap-5 sm:max-w-96 sm:justify-center">
-        <div className="flex w-full flex-col items-start justify-center gap-2 sm:items-center">
+      <div className="flex w-full flex-col items-center justify-start sm:max-w-96 sm:justify-center">
+        <div className="mb-10 flex w-full flex-col items-start justify-center gap-2 sm:items-center">
           <span className="text-lg text-neutral-900">이게뭐약 사용자 회원 정보 수정</span>
           <h1 className="text-5xl font-semibold">내 정보 수정</h1>
         </div>
