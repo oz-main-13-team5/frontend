9b76cca41ff07b41947f3a87f961a09eb123587a
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-10 15:18:17 +0900

    feat: 소셜 로그인 사용자는 비밀번호 변경 숨김 (#84)

--- src/pages/MyPageEdit.tsx
+++ src/pages/MyPageEdit.tsx
@@ -1,8 +1,20 @@
 import Tab from "@/components/common/Tab";
 import NicknameEditForm from "@/components/my-page/edit/NicknameEditForm";
 import PasswordEditForm from "@/components/my-page/edit/PasswordEditForm";
+import { useAuthStore } from "@/hooks/stores/useAuthStore";
+import { useEffect, useState } from "react";
 
 export default function MyPageEdit() {
+  const [isSocialLogin, setIsSocialLogin] = useState(true);
+
+  const user = useAuthStore((state) => state.user);
+
+  useEffect(() => {
+    if (user && user.provider) {
+      setIsSocialLogin(false);
+    }
+  }, [user]);
+
   return (
     <div className="flex justify-center p-5">
       <div className="flex w-full flex-col items-center justify-start sm:max-w-96 sm:justify-center">
@@ -10,13 +22,18 @@ export default function MyPageEdit() {
           <span className="text-lg text-neutral-900">이게뭐약 사용자 회원 정보 수정</span>
           <h1 className="text-5xl font-semibold">내 정보 수정</h1>
         </div>
-        <Tab
-          items={[
-            { key: "nickname", label: "닉네임 수정", content: <NicknameEditForm /> },
-            { key: "password", label: "비밀번호 수정", content: <PasswordEditForm /> },
-          ]}
-          className="mb-5 w-full"
-        />
+
+        {isSocialLogin ? (
+          <NicknameEditForm />
+        ) : (
+          <Tab
+            items={[
+              { key: "nickname", label: "닉네임 수정", content: <NicknameEditForm /> },
+              { key: "password", label: "비밀번호 수정", content: <PasswordEditForm /> },
+            ]}
+            className="mb-5 w-full"
+          />
+        )}
       </div>
     </div>
   );
