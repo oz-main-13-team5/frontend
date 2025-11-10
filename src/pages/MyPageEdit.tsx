5b432d05647a4551234b693c67ed3161d579f0e8
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-10 16:09:11 +0900

    fix: import문 default로 수정 (#84)

--- src/pages/MyPageEdit.tsx
+++ src/pages/MyPageEdit.tsx
@@ -1,7 +1,7 @@
 import Tab from "@/components/common/Tab";
 import NicknameEditForm from "@/components/my-page/edit/NicknameEditForm";
 import PasswordEditForm from "@/components/my-page/edit/PasswordEditForm";
-import { useAuthStore } from "@/hooks/stores/useAuthStore";
+import useAuthStore from "@/hooks/stores/useAuthStore";
 import { useEffect, useState } from "react";
 
 export default function MyPageEdit() {
@@ -10,7 +10,7 @@ export default function MyPageEdit() {
   const user = useAuthStore((state) => state.user);
 
   useEffect(() => {
-    if (user && user.provider) {
+    if (user && !user.provider) {
       setIsSocialLogin(false);
     }
   }, [user]);
