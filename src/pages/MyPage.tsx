54007527fa68ba3328d47cd1acda814191073c41
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-11 00:54:03 +0900

    feat: 마이페이지 북마크 리스트 조회 훅 useBookmarkList 추가 및 렌더링 구현 (#85)

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,11 +1,16 @@
 import Button from "@/components/common/Button";
 import Tab from "@/components/common/Tab";
+import PillListItem from "@/components/PillListItem";
+import useBookmarkList from "@/hooks/api/my-page/useBookmarkList";
 import { Link } from "react-router";
 
 export default function Mypage() {
+  const { data } = useBookmarkList();
+  const bookmarkedPills = data?.pills ?? [];
+
   return (
     <div className="flex w-full justify-center gap-5 pt-10 sm:pt-20">
-      <div className="flex w-full max-w-[1440px] flex-col items-start gap-10 sm:flex-row">
+      <div className="flex w-full max-w-[1440px] flex-col items-start sm:flex-row">
         {/* 유저 프로필 */}
         <div className="grid w-full gap-2 px-10 text-center sm:w-auto">
           <p className="text-2xl font-medium text-neutral-900">이건뭐약</p>
@@ -26,11 +31,11 @@ export default function Mypage() {
         </div>
 
         {/* 리스트 렌더링 */}
-        <div className="w-full flex-1 px-3 sm:px-0">
-          <h2 className="pb-5 text-2xl font-medium text-neutral-900 sm:text-5xl sm:font-semibold">
+        <div className="w-full flex-1 px-3 sm:px-5">
+          <h2 className="pt-10 pb-5 text-2xl font-medium text-neutral-900 sm:pt-0 sm:text-5xl sm:font-semibold">
             북마크 목록
           </h2>
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center gap-3 sm:gap-10">
             <Tab
               className="w-full max-w-[640px] rounded-4xl p-1"
               items={[
@@ -38,9 +43,13 @@ export default function Mypage() {
                   key: "bookmark",
                   label: "북마크",
                   content: (
-                    <div>
-                      <p>북마크</p>
-                      {/* 북마크 리스트 */}
+                    <div className="grid w-full gap-5">
+                      {bookmarkedPills.length === 0 && (
+                        <p className="text-center text-neutral-500">북마크된 약이 없습니다.</p>
+                      )}
+                      {bookmarkedPills.map((pill) => (
+                        <PillListItem key={pill.item_seq} pill={pill} />
+                      ))}
                     </div>
                   ),
                   activeClassName: "rounded-2xl",
