04f603ef0885f55910fbf19c730d4849434d70e0
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-10 13:46:20 +0900

    feat: 의약품 상세 정보 인터페이스 (#89)

--- src/types/api-response-types/pill-response-types.ts
+++ src/types/api-response-types/pill-response-types.ts
@@ -13,3 +13,11 @@ export interface Pill {
   item_image_url: string;
   is_marked: boolean;
 }
+
+export interface PillDetail extends Pill {
+  use_method_qesitm: string; //복용방법
+  atpn_warn_qesitm: string; //복용 시 주의사항
+  intrc_qesitm: string; //상호작용 정보 (다른 약, 술, 담배 등)
+  se_qesitm: string; //부작용 정보
+  deposit_method_qesitm: string; //보관 방법
+}
