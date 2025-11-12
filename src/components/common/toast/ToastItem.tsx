203d97fb26c7151739b2d76584369650873734fe
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-12 14:54:12 +0900

    feat: ToastItem 컴포넌트 (#125)

---
+++ src/components/common/toast/ToastItem.tsx
@@ -0,0 +1,59 @@
+import useToastStore from "@/hooks/stores/useToastStore";
+import { cn } from "@/libs/utils";
+import type { Toast } from "@/types/types";
+import { CircleCheckIcon, CircleXIcon, XIcon } from "lucide-react";
+
+const TOAST_COLORS = {
+  success: {
+    border: "border-green-300",
+    bg: "bg-green-50",
+    icon: "text-green-500",
+    title: "text-green-800",
+    content: "text-green-500",
+  },
+
+  error: {
+    border: "border-red-300",
+    bg: "bg-red-50",
+    icon: "text-red-500",
+    title: "text-red-800",
+    content: "text-red-500",
+  },
+};
+
+const TOAST_ICON = {
+  success: CircleCheckIcon,
+  error: CircleXIcon,
+};
+
+function ToastItem({ id, type, title, content }: Toast) {
+  const styles = TOAST_COLORS[type];
+  const Icon = TOAST_ICON[type];
+  const { removeToast } = useToastStore();
+
+  const handleClickDelete = () => {
+    removeToast(id);
+  };
+
+  return (
+    <div
+      className={cn(
+        "animate-fade-in-out-toast flex justify-between gap-3 rounded-lg border border-solid p-4 opacity-0",
+        styles.border,
+        styles.bg
+      )}
+    >
+      <Icon className={cn("relative bottom-0.5 w-4.5", styles.icon)} />
+      <div className="flex-1 text-sm">
+        <h4 className={styles.title}>{title}</h4>
+        <p className={styles.content}>{content}</p>
+      </div>
+      <XIcon
+        className={cn("relative bottom-0.5 w-4.5 cursor-pointer", styles.icon)}
+        onClick={handleClickDelete}
+      />
+    </div>
+  );
+}
+
+export default ToastItem;
