f60355e8a18d2f99fbce2ca763aa244810a72fb8
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-12 14:36:03 +0900

    feat: Toast 타입 생성 (#125)

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,2 +1,9 @@
 export type pillSearchOptionFrontend = "itemName" | "enterpriseName" | "efficacy";
 export type pillSearchOptionBackend = "item_name" | "entp_name" | "efcy_qesitm";
+
+export interface Toast {
+  id: number;
+  type: "success" | "error";
+  title: string;
+  content: string;
+}
