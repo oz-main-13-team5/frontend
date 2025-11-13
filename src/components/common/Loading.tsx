2782a363fb5193346842b05b5090ed6487ae6971
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-13 13:37:55 +0900

    feat: loading component (#112)

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,8 +2,20 @@ import { cn } from "@/libs/utils";
 
 interface LoadingProps {
   className?: string;
+  spinnerClassName?: string;
 }
 
-export default function Loading({ className }: LoadingProps) {
-  return <div className={cn("inline", className)}>Loading</div>;
+export default function Loading({ className, spinnerClassName }: LoadingProps) {
+  return (
+    <div className={cn("inline-flex items-center gap-2", className)}>
+      {/* 스피너 */}
+      <div
+        className={cn(
+          "size-7 animate-spin rounded-full border-2 border-green-500 border-t-transparent",
+          spinnerClassName
+        )}
+        role="status"
+      />
+    </div>
+  );
 }
