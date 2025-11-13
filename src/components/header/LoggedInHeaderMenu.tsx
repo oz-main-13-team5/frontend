bf9e0bb3cf4365ea754da5ee5c61c43ed2a3f670
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-13 17:20:44 +0900

    fix: 모바일 Header 로그아웃 버튼 onClick 누락 수정 (#139)

--- src/components/header/LoggedInHeaderMenu.tsx
+++ src/components/header/LoggedInHeaderMenu.tsx
@@ -28,6 +28,7 @@ export default function LoggedInHeaderMenu() {
         size="sm"
         className="inline-flex h-10 w-10 items-center justify-center p-0 text-green-600 sm:hidden"
         aria-label="로그아웃"
+        onClick={handleLogout}
       >
         <LogOutIcon />
       </Button>
