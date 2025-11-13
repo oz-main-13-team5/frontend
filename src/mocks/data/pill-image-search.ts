24b6b640fbf85d1be702019e7c7ee41160e8a1af
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-13 13:58:13 +0900

    rename: 이미지 검색 요청 목록 목데이터 mockPillImageSearchRecords로 분리 (#128)

---
+++ src/mocks/data/pill-image-search.ts
@@ -0,0 +1,23 @@
+export const mockPillImageSearchRecords = [
+  {
+    filename: "pill_pending.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
+    status: "pending",
+    created_at: "25111310121532",
+    item_seq: "",
+  },
+  {
+    filename: "pill_ok.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
+    status: "completed",
+    created_at: "25111210112152",
+    item_seq: "100001", // 타이레놀
+  },
+  {
+    filename: "pill_fail.jpg",
+    url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
+    status: "completed_failed",
+    created_at: "25111110112152",
+    item_seq: "",
+  },
+];
--- src/mocks/handlers/my-page-handlers.ts
+++ src/mocks/handlers/my-page-handlers.ts
@@ -1,5 +1,6 @@
 import { MSW_BASE_URL } from "@/constants/url-constants";
 import { mockPills } from "@/mocks/data/pill-data";
+import { mockPillImageSearchRecords } from "@/mocks/data/pill-image-search";
 import { http, HttpResponse } from "msw";
 
 const BOOKMARK_LIMIT = 20;
@@ -41,26 +42,7 @@ const getBookmarks = http.get(`${MSW_BASE_URL}/bookmark`, () => {
 const getImageSearchList = http.get(`${MSW_BASE_URL}/my_requests`, () => {
   return HttpResponse.json(
     {
-      records: [
-        {
-          filename: "pill_pending.jpg",
-          url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
-          status: "pending",
-          item_seq: "",
-        },
-        {
-          filename: "pill_ok.jpg",
-          url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
-          status: "completed",
-          item_seq: "100001", // 타이레놀
-        },
-        {
-          filename: "pill_fail.jpg",
-          url: "https://plus.unsplash.com/premium_photo-1668487826871-2f2cac23ad56?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JUVDJTk1JUJEfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=900",
-          status: "completed_failed",
-          item_seq: "",
-        },
-      ],
+      records: mockPillImageSearchRecords,
     },
     { status: 200 }
   );
