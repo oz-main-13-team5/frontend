c8b42ea909d342e2d17fa6c6a17bbc15be6979ad
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-14 15:47:19 +0900

    fix: 실제 성능 개선 없음으로 판단되어 적용 취소 (#149)

--- src/components/PillListItem.tsx
+++ src/components/PillListItem.tsx
@@ -1,5 +1,4 @@
 import Bookmark from "@/components/common/Bookmark";
-import Image from "@/components/common/Image";
 import { cn } from "@/libs/utils";
 import type { Pill } from "@/types/api-response-types/pill-response-types";
 import { ChevronRightIcon } from "lucide-react";
@@ -26,7 +25,7 @@ export default function PillListItem({ className, pill }: PillListItemProps) {
         className
       )}
     >
-      <Image
+      <img
         alt={`${name}-image`}
         src={imageUrl}
         className="h-20 w-20 object-contain object-center sm:h-24 sm:w-36"
