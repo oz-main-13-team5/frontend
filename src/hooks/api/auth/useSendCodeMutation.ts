ed4268a1da8c02e3794a67fadd78426374242e53
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-17 13:17:21 +0900

    fix: 회원가입 인증코드 엔드포인트 수정 (/users/signup/send/) (#155)

--- src/hooks/api/auth/useSendCodeMutation.ts
+++ src/hooks/api/auth/useSendCodeMutation.ts
@@ -23,7 +23,7 @@ export default function useSendCodeMutation(
           SignUpSendResponse,
           AxiosResponse<SignUpSendResponse>,
           SignUpSendRequest
-        >("/auth/email_send/", payload, { baseURL: API_BASE_URL })
+        >("/users/signup/send/", payload, { baseURL: API_BASE_URL })
         .then((res) => res.data),
     ...options,
   });
