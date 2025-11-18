317b5ee826a41bee4d48c2093d6cb1295f8a91de
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-18 13:33:40 +0900

    fix: 의약품 제목 너무 길면 ... 처리(#173)

--- src/components/PillListItem.tsx
+++ src/components/PillListItem.tsx
@@ -31,19 +31,19 @@ export default function PillListItem({ className, pill }: PillListItemProps) {
         src={imageUrl || defautPillImage}
         className="h-20 w-20 rounded-lg object-cover object-center sm:size-30"
       />
-      <div className="flex flex-1 flex-col items-start justify-center gap-2 sm:gap-3">
+      <div className="flex flex-1 flex-col items-start justify-center gap-5 sm:gap-3">
         <div className="flex w-full items-center justify-between">
           <Link
             to={`/pill/${id}`}
             aria-label={`${name} 상세페이지`}
-            className="flex items-center gap-0.5"
+            className="flex min-w-0 items-center gap-0.5"
           >
-            <span className="text-lg font-normal text-neutral-900 sm:text-2xl sm:font-medium">
+            <span className="line-clamp-1 text-lg font-normal text-ellipsis text-neutral-900 sm:text-2xl sm:font-medium">
               {name}
             </span>
-            <ChevronRightIcon className="h-6 w-6 text-neutral-400" />
+            <ChevronRightIcon className="h-6 w-6 shrink-0 text-neutral-400" />
           </Link>
-          <Bookmark pillId={id} initialState={isMarked} />
+          <Bookmark pillId={id} initialState={isMarked} className="shrink-0" />
         </div>
         <p className="line-clamp-1 text-lg text-ellipsis text-neutral-600">{description}</p>
       </div>
