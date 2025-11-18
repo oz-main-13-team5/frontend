91262e84c2b45941332b7293eac586005571ec2d
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-18 13:20:51 +0900

    rename: 이미지 파일들을 public 폴더에서 assets 폴더로 이동 (#173)

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,6 +5,7 @@ import useAuthStore from "@/hooks/stores/useAuthStore";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import logoImage from "@/assets/images/logo/logo.png";
 
 export default function Header() {
   const { isAuthed } = useAuthStore();
@@ -24,7 +25,7 @@ export default function Header() {
         <div className="flex w-full max-w-[1440px] items-center justify-between p-3">
           <h1>
             <Link to="/" className="text-lg text-neutral-50 sm:text-2xl sm:font-medium">
-              <img src="/logo/logo.png" alt="이건뭐약 로고" className="h-10 w-auto sm:h-14" />
+              <img src={logoImage} alt="이건뭐약 로고" className="h-10 w-auto sm:h-14" />
             </Link>
           </h1>
 
--- src/components/social-login-button/KakaoButton.tsx
+++ src/components/social-login-button/KakaoButton.tsx
@@ -1,3 +1,5 @@
+import kakaoImage from "@/assets/images/social-icons/kakao.svg";
+
 export default function KakaoButton({ onClick }: { onClick?: () => void }) {
   return (
     <button
@@ -5,7 +7,7 @@ export default function KakaoButton({ onClick }: { onClick?: () => void }) {
       onClick={onClick}
       className="relative flex h-14 flex-1 cursor-pointer items-center justify-center rounded-xl bg-[#FEE500] p-2 transition hover:bg-[#F7D700] active:bg-[#E6C600]"
     >
-      <img src="/social-icons/kakao.svg" alt="kakao login" className="absolute left-4 h-5 w-5" />
+      <img src={kakaoImage} alt="kakao login" className="absolute left-4 h-5 w-5" />
       <span className="ml-6 text-sm font-medium text-neutral-900">카카오 로그인</span>
     </button>
   );
