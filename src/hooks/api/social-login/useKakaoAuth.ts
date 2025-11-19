4ee97689f92b8981c91d75d80fe78291dfaf3405
hyebinpark <hoonywhitesocks@gmail.com>
2025-11-19 12:47:29 +0900

    feat: 카카오 api 연결 (#104)

--- src/hooks/api/social-login/useKakaoAuth.ts
+++ src/hooks/api/social-login/useKakaoAuth.ts
@@ -1,9 +1,10 @@
+import { API_BASE_URL } from "@/constants/url-constants";
 import { api } from "@/libs/axios";
 
 // 카카오 소셜 로그인
 export const kakaoLogin = async () => {
   try {
-    const response = await api.get("/auth/social/kakao/login");
+    const response = await api.get("/auth/social/kakao/login", { baseURL: API_BASE_URL });
 
     if (response.data?.auth_url) {
       window.location.href = response.data.auth_url;
