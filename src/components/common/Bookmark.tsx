b6b74eb35d1c2e3c7def38d5dcec51db4035824a
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-11-19 14:56:32 +0900

    fix: 북마크 최대 개수 초과 경고 모달에서 토스트로 변경 (#168)

--- src/components/common/Bookmark.tsx
+++ src/components/common/Bookmark.tsx
@@ -1,4 +1,3 @@
-import Modal from "@/components/Modal";
 import useToggleBookmark from "@/hooks/api/useToggleBookmark";
 import useAuthStore from "@/hooks/stores/useAuthStore";
 import useToast from "@/hooks/useToast";
@@ -18,7 +17,6 @@ export default function Bookmark({
   ...props
 }: BookmarkProps) {
   const [isBookmarked, setIsBookmarked] = useState(initialState);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const { isAuthed } = useAuthStore((state) => state);
   const { triggerToast } = useToast();
 
@@ -29,7 +27,7 @@ export default function Bookmark({
         //실패하면 낙천적 UI 정정
         if (data && !data?.success) {
           setIsBookmarked((prev) => !prev);
-          setIsModalOpen(true);
+          triggerToast("error", "북마크 최대 개수 초과", "북마크는 최대 20개까지 가능합니다.");
         }
       },
     }
@@ -62,15 +60,6 @@ export default function Bookmark({
       >
         <BookmarkIcon className="text-green-600" fill={isBookmarked ? "#16a34a" : "#fafafa"} />
       </button>
-
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => {
-          setIsModalOpen(false);
-        }}
-        title="북마크 최대 개수 초과"
-        description="북마크는 최대 20개까지만 가능합니다."
-      />
     </>
   );
 }
